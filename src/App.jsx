e7ea68ca1ee7354dc5142ceb760d46908457eea7
Shahrzad Baghestani <shahrzadbaghestani@gmail.com>
2025-02-28 22:34:30 -0500

    Update App.jsx
    Added social sites

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Shahrzad Baghestani",
+  title: "Software Developer",
+  email: "shahrzadbg98@gmail.com",
+  gitHub: "https://ghttps://github.com/shahrzadbst",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/shahrzad-baghestani/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
